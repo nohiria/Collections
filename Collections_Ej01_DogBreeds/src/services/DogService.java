28eba5a4569bbde214667233b2b571f7e5c68090
Nohiria Velazquez <nohiriavg@gmail.com>
2023-07-26 17:55:19 -0500

    Excercise 01 addDog method changes

--- Collections_Ej01_DogBreeds/src/services/DogService.java
+++ Collections_Ej01_DogBreeds/src/services/DogService.java
@@ -20,11 +20,16 @@ public class DogService {
         dogsList= new ArrayList<>();
     }
     
-    public Dog addDog(String breed){
-        System.out.println("Adding "+breed+" to the list");
-        Dog d= new Dog(breed);
-        dogsList.add(d);
-        return d;
+    public Dog addDog(String breed) {
+        if (dogsList.stream().anyMatch(d -> d.getBreed().equalsIgnoreCase(breed))) {
+            System.out.println(breed + " is already in the list");
+            return null;
+        } else {
+            System.out.println("Adding " + breed + " to the list");
+            Dog d = new Dog(breed);
+            dogsList.add(d);
+            return d;
+        }
     }
     
     public void showDogsList(){
@@ -40,7 +45,11 @@ public class DogService {
         do{
             System.out.println("Enter the breed of the dog:");
             String breed= input.nextLine();
-            addDog(breed);
+            Dog addedDog= addDog(breed);
+            
+            if(addedDog!=null){
+                System.out.println("Successfully added: " + addedDog.getBreed());
+            }
             
             System.out.print("Do you want to add another dog? (y/n): ");
             choice = input.nextLine();
