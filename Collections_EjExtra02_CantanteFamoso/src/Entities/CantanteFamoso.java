675174190e38f3a012fc1aa9471e24dd120fc6e6
Nohiria Velazquez <nohiriavg@gmail.com>
2023-07-29 20:25:32 -0500

    Update

---
+++ Collections_EjExtra02_CantanteFamoso/src/Entities/CantanteFamoso.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entities;
+
+/**
+ *
+ * @author nohyv
+ */
+public class CantanteFamoso {
+    private String nombre;
+    private String discoConMasVentas;
+    
+    //Constructores
+    public CantanteFamoso() {
+    }
+
+    public CantanteFamoso(String nombre, String discoConMasVentas) {
+        this.nombre = nombre;
+        this.discoConMasVentas = discoConMasVentas;
+    }
+    
+    //Getters&Setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDiscoConMasVentas() {
+        return discoConMasVentas;
+    }
+
+    public void setDiscoConMasVentas(String discoConMasVentas) {
+        this.discoConMasVentas = discoConMasVentas;
+    }
+    
+}
---
+++ Collections_EjExtra03_Libreria/src/Entidad/Libro.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidad;
+
+/**
+ *
+ * @author nohyv
+ */
+public class Libro {
+    private String titulo;
+    private String autor;
+    private int numEjemplares;
+    private int numEjemPrestados;
+}
