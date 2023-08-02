cdc75ef3055d11792726d3bd1abb1b84a1fb7994
Nohiria Velazquez <nohiriavg@gmail.com>
2023-08-02 18:36:28 -0500

    Excercise 03 crearAlumno method

--- Collections_Ej03_Alumno/src/Entidad/Alumno.java
+++ Collections_Ej03_Alumno/src/Entidad/Alumno.java
@@ -5,10 +5,37 @@
  */
 package Entidad;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author nohyv
  */
 public class Alumno {
+    private String nombre;
+    private ArrayList<Integer> notas;
+    
+    //Constructor
+    public Alumno() {
+        notas= new ArrayList<>();
+    }
+    
+    
+    //Getters&Setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public ArrayList<Integer> getNotas() {
+        return notas;
+    }
+
+    public void setNotas(ArrayList<Integer> notas) {
+        this.notas = notas;
+    }
     
 }
--- Collections_Ej03_Alumno/src/Servicio/ServicioAlumno.java
+++ Collections_Ej03_Alumno/src/Servicio/ServicioAlumno.java
@@ -5,10 +5,58 @@
  */
 package Servicio;
 
+import Entidad.Alumno;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author nohyv
  */
 public class ServicioAlumno {
+    private final ArrayList<Alumno> listaDeAlumnos;
+    private final Scanner input;
+
+    public ServicioAlumno() {
+        listaDeAlumnos= new ArrayList<>();
+        input= new Scanner(System.in);
+    }
+   
+    //Método para crear alumno
+    public Alumno crearALumno(){
+        Alumno alumno= new Alumno();
+        ArrayList<Integer> notas= new ArrayList<>();
+        
+        System.out.println("Ingresa el nombre del alumno");
+        alumno.setNombre(input.nextLine());
+        System.out.println("Ingresa la primera nota");
+        notas.add(Integer.parseInt(input.nextLine()));
+        System.out.println("Ingresa la segunda nota");
+        notas.add(Integer.parseInt(input.nextLine()));
+        System.out.println("Ingresa la tercera nota");
+        notas.add(Integer.parseInt(input.nextLine()));
+        alumno.setNotas(notas);
+        
+        return alumno;
+    }
     
+    //Añadir alumno a la lista
+    public void agregarAlumno(Alumno alumno){
+        listaDeAlumnos.add(alumno);
+    }
+    
+    //Bucle para agregar alumnos
+    public void crearListaAlumnos(){
+        System.out.println("¿Desea agregar un alumno a la lista de alumnos?");
+        String respuesta= input.nextLine();
+        while(respuesta.equalsIgnoreCase("y")){
+            agregarAlumno(crearALumno());
+            System.out.println("¿Desea agregar otro alumno a la lista?");
+            respuesta= input.nextLine();
+        }
+    }
+    
+    public void notaFinal(){
+        
+    }
 }
