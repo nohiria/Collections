8dcd66fbff5aa5caf3969f463ef1fc1fa3965764
Nohiria Velazquez <nohiriavg@gmail.com>
2023-08-07 14:35:55 -0500

    Excercise 04 Comparators Update

--- Collections_Ej04_Peliculas/src/Utilidades/Comparators.java
+++ Collections_Ej04_Peliculas/src/Utilidades/Comparators.java
@@ -13,19 +13,35 @@ import java.util.Comparator;
  * @author nohyv
  */
 public class Comparators {
-    public static Comparator<Pelicula> ordenarDuracionDeMenorAMayor= new Comparator<Pelicula>() {
-        @Override
-        public int compare(Pelicula o1, Pelicula o2) {
-            return o1.getHorasDuracion().compareTo(o2.getHorasDuracion());
-        }
-    };
     
-    public static Comparator<Pelicula> ordenarDuracionDeMayorAMenor= new Comparator<Pelicula>() {
-        @Override
-        public int compare(Pelicula o1, Pelicula o2) {
-            return o2.getHorasDuracion().compareTo(o1.getHorasDuracion());
-        }
-    };
+    //Según duración
+        public static Comparator<Pelicula> ordenarDuracionDeMenorAMayor= new Comparator<Pelicula>() {
+            @Override
+            public int compare(Pelicula o1, Pelicula o2) {
+                return o1.getHorasDuracion().compareTo(o2.getHorasDuracion());
+            }
+        };
+
+        public static Comparator<Pelicula> ordenarDuracionDeMayorAMenor= new Comparator<Pelicula>() {
+            @Override
+            public int compare(Pelicula o1, Pelicula o2) {
+                return o2.getHorasDuracion().compareTo(o1.getHorasDuracion());
+            }
+        };
     
     
+    //Según título
+        public static Comparator<Pelicula> ordenarTituloAlfabeticamente= new Comparator<Pelicula>() {
+            @Override
+            public int compare(Pelicula o1, Pelicula o2) {
+                return o1.getTitulo().compareTo(o2.getTitulo());
+            }
+        };
+        
+        public static Comparator<Pelicula> ordenarTituloAlfabeticamenteRev= new Comparator<Pelicula>() {
+            @Override
+            public int compare(Pelicula o1, Pelicula o2) {
+                return o2.getTitulo().compareTo(o1.getTitulo());
+            }
+        };
 }
